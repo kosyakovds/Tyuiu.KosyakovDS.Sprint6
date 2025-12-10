e1e591da36dafee91edd930923d7f8232c531ff6
kosyakovds <kosyakov27@mail.ru>
2025-12-10 23:50:48 +0500

    Task4 V18

--- Tyuiu.KosyakovDS.Sprint6.Task4.V18.Test/DataServiceTest.cs
+++ Tyuiu.KosyakovDS.Sprint6.Task4.V18.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.KosyakovDS.Sprint6.Task4.V18.Test
             int start = -5;
             int stop = 5;
             DataService ds = new DataService();
-            double[] wait = { 7.76, 7.36, 7.26, 7.9, 0, 2, 4.35, 5.63, 6.49, 6.49, 6.32 };
+            double[] wait = { 7.76, 7.36, 7.26, 7.9, 15.88, 2, 4.35, 5.63, 6.49, 6.49, 6.32 };
             double[] res = ds.GetMassFunction(start, stop);
             CollectionAssert.AreEqual(wait, res);
         }
